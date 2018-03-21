f6ccb5ff44949ab7322148b642f225e1c90de2c0
Valerie Bug Warner <valerie.e.warner@gmail.com>
2018-03-21 14:23:37 -0600

    created context processor and updated some files to access global django vars.

--- gui/settings.py
+++ gui/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'gui.context_processors.global_settings',
 ]
 
 ROOT_URLCONF = 'gui.urls'
