e53569bd86061a9968b84281c52123782e837089
Valerie Warner <valerie.e.warner@gmail.com>
2018-03-21 14:30:59 -0600

    fixed typo

--- gui/settings.py
+++ gui/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'gui.context_processors.global_settings',
 ]
 
 ROOT_URLCONF = 'gui.urls'
@@ -76,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'gui.context_processor.global_settings',
             ],
         },
     },
